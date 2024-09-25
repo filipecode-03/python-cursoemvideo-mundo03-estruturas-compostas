a7123a043753d4883c525b86504a7d48d53b1043
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-09-25 15:38:26 -0400

    retomando desafios

--- Aula 17/ex03.py
+++ Aula 17/ex03.py
@@ -3,18 +3,25 @@ num[2] = 3
 print(num)
 
 num2 = num[:] 
-num.append(7)
+num2.append(7)
 print(num2)
 
 num3 = num[:]
-num.sort(reverse=True)
+num3.sort(reverse=True)
 print(num3)
-num.insert(2, 2)
-num.pop(2)
-if 5 in num:
-    num.remove(5)
+
+num4 = num[:]
+num4.insert(2, 2)
+print(num4)
+
+num5 = num[:]
+num5.pop(2)
+print(num5)
+
+num6 = num[:]
+if 5 in num6:
+    num6.remove(5)
+    print(num6)
 else:
     print('Não achei o número 5')
-num.remove(4)
-print(num)
 print(f'Essa lista tem {len(num)} elementos')
