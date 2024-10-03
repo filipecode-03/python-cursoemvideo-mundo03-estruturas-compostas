79ba412298d4c46024131b02f7c284ad530f92ba
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-10-03 09:02:08 -0400

    finalizei os desafios da aula 20, e com isso realizei mais de 100 desafios em python

--- Exercicios/ex100.py
+++ Exercicios/ex100.py
@@ -1,13 +1,19 @@
 from random import randint
 from time import sleep
 números = []
-def sorteia():
+def sorteia(lista):
     print(f'Sorteando 5 valores da lista: ', end='')
     for cont in range(0, 5):
         n = randint(1, 10)
-        números.append(n)
+        lista.append(n)
         print(f'{n} ', end='', flush=True)
         sleep(0.3)
     print('PRONTO!')
-# def somaPar():
+def somaPar(numpar):
+    s = 0
+    for cont in numpar:
+        if cont % 2 == 0:
+            s += cont
+    print(f'Somando os valores pares de {numpar}, temos {s}') 
 sorteia(números)
+somaPar(números)
