f02cf25a4a02386411b96d98a86906d461912f3a
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-10-03 10:00:33 -0400

    pela manhã é só

---
+++ Aula 21/escopo de variáveis/ex01.py
@@ -0,0 +1,8 @@
+def teste():
+    x = 8
+    print(f'Na função teste, n vale {n}')
+    print(f'Na função teste, x vale {x}')
+n = 2
+print(f'No programa principal, n vale {n}')
+teste()
+print(f'No programa principal, x vale {x}')
---
+++ Aula 21/escopo de variáveis/ex02.py
@@ -0,0 +1,8 @@
+def teste(b):
+    b += 4
+    c = 2
+    print(f'A dentro vale {a}')
+    print(f'B dentro vale {b}')
+    print(f'C dentro vale {c}')
+a = 5
+teste(a)
