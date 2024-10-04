b830cf2ad0ce1b9e6ae9cf81935b7e8dd0fbad6a
filipeoliveira-03 <luiz.fmoc0311@gmail.com>
2024-10-04 11:42:39 -0400

    pela manhã é só

---
+++ Exercicios/ex107.py
@@ -0,0 +1,5 @@
+import ex107moeda
+valor = float(input('Digite o preço: '))
+print(f'A metade de R${valor:.1f} é R${ex107moeda.metade(valor)}')
+print(f'O dobro de R${valor:.1f} é R${ex107moeda.dobro(valor)}')
+print(f'Aumentando 10%, temos R${ex107moeda.aumentar(valor)}')
---
+++ Exercicios/ex107moeda.py
@@ -0,0 +1,15 @@
+def aumentar(num): 
+    aumento = num * (10 / 100)
+    novo_valor = num + aumento
+    return novo_valor
+
+def diminuir(num):
+    desconto = num * (10 / 100)
+    novo_valor = num - desconto
+    return novo_valor
+ 
+def dobro(num):
+    return num * 2
+
+def metade(num):
+    return num / 2
